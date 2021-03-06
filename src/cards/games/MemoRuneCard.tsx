c8819dbf2daa3c5c2d4655f4a1fabd26970e53d7
Matt Newbigging <mnewbigging@opus2.com>
2021-03-06 17:05:20 +0000

    fixed button click area

--- src/cards/games/MemoRuneCard.tsx
+++ src/cards/games/MemoRuneCard.tsx
@@ -10,21 +10,12 @@ export class MemoRuneCard extends React.PureComponent {
   public render() {
     return (
       <Card
-        logo={this.renderLogo()}
+        logo={<div className={'mr-logo'}></div>}
         title={'Memo Rune'}
         icons={[<TwoPlayerIcon />, <LocalIcon />]}
         blurb={'Pair matching runes, with penalties for matching on certain dangerous runes.'}
         link={'https://mnewbigging.github.io/memo-rune'}
       />
-    );
-  }
-
-  private renderLogo() {
-    return (
-      <div className={'mr-logo'}>
-        <div>MEMO</div>
-        <div>RUNE</div>
-      </div>
     );
   }
 }
